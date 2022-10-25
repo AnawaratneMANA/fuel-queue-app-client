516f35bb1520f4ac878781984b72e990b8687cc4
AnawaratneMANA <it19086408@my.sliit.lk>
2022-10-26 00:39:23 +0530

    Add Approval status toggle to fuel requests

---
+++ app/src/main/java/com/example/fuelqueueapplication/api/request/ApprovalStatusUpdateRequest.java
@@ -0,0 +1,13 @@
+package com.example.fuelqueueapplication.api.request;
+
+public class ApprovalStatusUpdateRequest {
+    private String approvalStatus;
+
+    public String getApprovalStatus() {
+        return approvalStatus;
+    }
+
+    public void setApprovalStatus(String approvalStatus) {
+        this.approvalStatus = approvalStatus;
+    }
+}
